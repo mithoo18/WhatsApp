f01fb86da0baee131a2890d4dc0b2bb16c974026
Devanshu Gupta <devanshu.gupta@student.amity.edu>
2021-02-02 00:36:19 +0530

    Firebase Realtime Concept Implementation

--- app/src/main/java/com/example/whatsapp/Model/Users.java
+++ app/src/main/java/com/example/whatsapp/Model/Users.java
@@ -1,4 +1,71 @@
 package com.example.whatsapp.Model;
 
 public class Users {
+    String profilepic;
+    String userName;
+    String mail;
+    String userId;
+    String lastMessage;
+    String password;
+
+    public Users(String profilepic, String userName, String mail, String userId, String lastMessage,String password) {
+        this.profilepic = profilepic;
+        this.userName = userName;
+        this.mail = mail;
+        this.password = password;
+
+        this.userId = userId;
+        this.lastMessage = lastMessage;
+    }
+    //Empty Constructor
+    public Users(){}
+
+    //For SignUp
+    public Users(String userName,String mail,String password) {
+        this.userName = userName;
+        this.mail = mail;
+        this.password = password;
+    }
+
+
+
+    public String getProfilepic() {
+        return profilepic;
+    }
+
+    public void setProfilepic(String profilepic) {
+        this.profilepic = profilepic;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
 }
--- app/src/main/java/com/example/whatsapp/SignUpActivity.java
+++ app/src/main/java/com/example/whatsapp/SignUpActivity.java
@@ -1,12 +1,18 @@
 package com.example.whatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.whatsapp.Model.Users;
 import com.example.whatsapp.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -29,9 +35,30 @@ public class SignUpActivity extends AppCompatActivity {
         binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                auth.createUserWithEmailAndPassword(binding.etEmail.getText().toString(),binding.etPassword.getText().toString());
+                auth.createUserWithEmailAndPassword(binding.etEmail.getText().toString(),binding.etPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //STORING DATA IN MODEL CLASS 'USER'->'FIREBASE-REALTIME'
+                            //USER
+                            Users user = new Users(binding.etuserName.getText().toString(),binding.etEmail.getText().toString(),
+                                    binding.etPassword.getText().toString());
+                            //GET ID
+                            String id = task.getResult().getUser().getUid();
+                            //DATA -> STORE ACCORDING TO ID
+                            database.getReference().child("Users").child(id).setValue(user);
 
-               }
+
+
+                            Toast.makeText(SignUpActivity.this, "Task Completed", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(SignUpActivity.this, "Task Not-Completed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                });
+            }
         });
 
 
