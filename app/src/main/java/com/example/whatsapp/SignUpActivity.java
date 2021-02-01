003be012f1ba0242642a23f9c7fecdb6bb0cf7be
Devanshu Gupta <devanshu.gupta@student.amity.edu>
2021-02-01 21:42:26 +0530

    Added Firebase Email & Password Authentication and new Lib Boom

--- app/src/main/java/com/example/whatsapp/SignUpActivity.java
+++ app/src/main/java/com/example/whatsapp/SignUpActivity.java
@@ -3,12 +3,21 @@ package com.example.whatsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+
+import com.example.whatsapp.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        //BINDING VIEW
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
     }
 }
