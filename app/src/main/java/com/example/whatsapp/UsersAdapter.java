253975c60bb2dcae8fa6c61e418291d6fffe02fb
Devanshu Gupta <devanshu.gupta@student.amity.edu>
2021-02-09 15:34:03 +0530

    CREATED USERADAPTER TO LIST USER IN WHATSAPP

---
+++ app/src/main/java/com/example/whatsapp/UsersAdapter.java
@@ -0,0 +1,54 @@
+package com.example.whatsapp;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.whatsapp.Model.Users;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder>{
+
+    ArrayList<Users> list;
+    Context context;
+
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.sample_show_user,parent,false);
+        return new ViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Users users = list.get(position);
+        //VIEWHOLDER -> GENERAL IMAGE
+        Picasso.get().load(users.getProfilepic()).placeholder(R.drawable.ic_user).into(holder.image);
+        holder.userName.setText();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView image;
+        TextView userName,lastMessage;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.profile_image);
+            image = itemView.findViewById(R.id.userNameList);
+            image = itemView.findViewById(R.id.LastMessage);
+        }
+    }
+
+}
